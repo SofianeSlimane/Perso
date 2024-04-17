0d42ca7759d2a3eb4b3ab17d58c223bae86a202f
SofianeSlimane <sofianeslimane24@gmail.com>
2024-04-17 10:21:17 +0000

    Saving progress on execve exercise

--- simple_shell_test/execve/execls.c
+++ simple_shell_test/execve/execls.c
@@ -2,17 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 int main(void)
 {
-	int returnValue;
+	int id2, id3, id4;
+	int id = fork();
 	char *argv[] = {"/bin/ls", "-l", "/tmp", NULL};
+	int status;
+
+	if (id != 0)
+	{
+		 id2 = fork();
+		 if (id2 != 0)
+		 {
+			 id3 = fork();
+		 }
+		 if (id3 != 0)
+		 {
+			 id4 = fork();
+		 }
+	}
+	if (id != 0)
+	{
+		wait(&status);
+	}
+		 
+		
 	
-	returnValue = execve(argv[0], argv, NULL);
-	if (returnValue == -1)
+	
+	
+	if (execve(argv[0], argv, NULL) == -1)
 	{
 		printf("Error");
 	}
-	printf("After execve\n");
 	return (0);
 }
