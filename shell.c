1c7fd430dfd17309a89898f1e56626fc86345c77
SofianeSlimane <sofianeslimane24@gmail.com>
2024-04-20 00:35:11 +0000

    Saving work on simple shell exercise

--- shell.c
+++ shell.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
+#include<sys/wait.h>
 
 int main(void)
 {
@@ -10,31 +10,39 @@ int main(void)
 	char *token;
 	const char* delim = " \n";
 	size_t size = 0;
-	char *argv[] = {"/bin/ls", NULL};
+	char **argv = NULL;
 	int i = 0;
+	int status;
+	pid_t pid;
 	while(1)
 	{
 		printf("$ ");
+		pid = fork();
+		if (pid > 0)
+		{
+			wait(&status);
+		}
+
 		if (getline(&buffer, &size, stdin) == -1)
-		{
-			return (-1);
-		}
-		
+                {
+                        return (-1);
+                }
 		token = strtok(buffer, delim);
-		while (token != NULL)
-		{
-			argv[i] = token;
-			token = strtok(NULL, delim);
-			i++;
-		}
-		argv[i] = NULL;
+                while (token != NULL)
+                {
+                        argv[i] = token;
+                        token = strtok(NULL, delim);
+                        i++;
+                }
+                argv[i] = NULL;
+                if (pid == 0)
+                {
 
+                execve(argv[0], argv, NULL);
+                }
 		
-		if (execve(argv[0], argv, NULL) == -1)
-		{
-			perror("Error");
-			exit(0);
-		}
 	}
-
+	free(buffer);
+	return (0);
+		
 }
